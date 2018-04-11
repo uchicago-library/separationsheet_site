2085d8cd486f43c1feb41f01a87d657901c00a50
Tyler Danstrom <tdanstrom@uchicago.edu>
2018-04-11 14:59:10 -0500

    starting to add basic tests

--- tests/test_separationsheet_site.py
+++ tests/test_separationsheet_site.py
@@ -1,20 +1,42 @@
 import unittest
-import separationsheet_site
+from unittest.mock import patch
+
+import separationsheet_site import app
 
 
 class Tests(unittest.TestCase):
     def setUp(self):
         # Perform any setup that should occur
         # before every test
-        pass
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        self.app = app.test_client()
 
     def tearDown(self):
         # Perform any tear down that should
         # occur after every test
         pass
 
-    def testPass(self):
-        self.assertEqual(True, True)
+    def testGetRootPage(self):
+        response = self.app.get("/", follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def testGetListPage(self):
+        response = self.app.get("/list", follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def testGetBotPage(self):
+        response = self.app.get("/both", follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def testGetRemovalPage(self):
+        response = self.app.get("/removal", follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def testGetBarcodePage(self):
+        response = self.app.get("/barcode/foo", follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
 
 
 if __name__ == "__main__":
